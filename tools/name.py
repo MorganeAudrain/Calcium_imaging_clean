80518f521776fa479b58ccc6128da5df8be8fb1d
morgane <morgane.audrain@edu.ece.fr>
2020-03-05 11:17:49 +0100

    run_steps

--- tools/name.py
+++ tools/name.py
@@ -8,7 +8,28 @@ This program is for create a excel sheet where all the behavioral path are stora
 # Importation
 import pandas as pd
 
-# Read the excel
 df = pd.read_excel(r'calcium_analysis_checked_videos.xlsx')
-data=pd.DataFrame(df, columns= ['condition'])
-print(data)
+mouse=pd.DataFrame(df, columns= ['mouse'])
+date=pd.DataFrame(df, columns= ['date'])
+trial=pd.DataFrame(df, columns= ['trial'])
+datetime=pd.DataFrame(df, columns= ['timestamp'],dtype='float64')
+path=pd.DataFrame(df, columns= ['Calcium_video'])
+for i in range(0,len(mouse)):
+    mouse1 = mouse.iloc[i]
+    date1=date.iloc[i]
+    trail1=trial.iloc[i]
+    datetime1=datetime.iloc[i]
+    path0=path.iloc[i]
+    for p in path0:
+        for m in mouse1:
+            for t in trail1:
+                for d in date1:
+                    for dt in datetime1:
+
+                        file_name=f'{d}_{m}_trial{t}_{d}%.f_0000.avi' % dt
+                        print(file_name)
+                        path='/home/morgane/triffle/homes/evelien/Calcium imaging/'
+                        open(file_name)
+                        df['behavioral_path']=file_name
+                        df.to_excel('behavioral_path.xlsx')
+
